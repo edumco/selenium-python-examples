409d4db9b0895129c98f3e9423d7364b4c490f17
José Eduardo Montenegro <edu.pombos@hotmail.com>
2024-11-08 04:08:31 -0300

    Adddss new fixture to login

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 
 from config import config
+from pages.login_page import LoginPage
 
 
 @pytest.fixture
@@ -14,3 +15,14 @@ def driver():
     driver = config.get_webdriver()
     yield driver
     driver.quit()
+
+
+@pytest.fixture
+def logged_driver(driver):
+    """Gets a new browser from the 'config.py' hand it over to the test and then close it after the test is finished.
+
+    Yields:
+        selenium.webdriver: driver to the select browser by config.py
+    """
+    LoginPage(driver).login("demouser", "abc123").is_authenticated()
+    yield driver
