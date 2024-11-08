d97bbc13e15be530bf1c0f16ee7963488232d362
José Eduardo Montenegro <edu.pombos@hotmail.com>
2024-11-08 04:50:23 -0300

    Testing invoice

--- pages/invoice_detail_page.py
+++ pages/invoice_detail_page.py
@@ -17,70 +17,75 @@ class InvoiceDetailsPage:
         wait = DriverWait(self.driver, 8)  # wait page load
         wait.until(Condition.presence_of_element_located(self.INVOICE_BILLING_SECTION))
 
-    def extract_all(self):
-
+    def extract_all_details(self):
         invoice_data = {}
+        invoice_data["hotel"] = self._extract_hotel()
+        invoice_data["invoice_id"] = self._extract_invoice_id()
+        invoice_data["invoice_date"], invoice_data["invoice_due_date"] = (
+            self._extract_invoice_dates()
+        )
+        invoice_data.update(self._extract_booking_details())
+        invoice_data.update(self._extract_stay_details())
+        invoice_data["customer_details"] = self._extract_customer_details()
+        invoice_data.update(self._extract_billing_details())
+        return invoice_data
 
-        # Hotel
+    def _extract_hotel(self):
         hotel = self.driver.find_element(By.CSS_SELECTOR, "h4.mt-5")
-        invoice_data["hotel"] = hotel.text
+        return hotel.text
 
-        # Invoice ID
+    def _extract_invoice_id(self):
         invoice_id = self.driver.find_element(By.CSS_SELECTOR, ".mt-2")
-        invoice_id_text = invoice_id.text.split("#")[1].split(" ")[0]
-        invoice_data["invoice_id"] = invoice_id_text
+        return invoice_id.text.split("#")[1].split(" ")[0]
 
-        # Invoice Dates
+    def _extract_invoice_dates(self):
         invoice_date = self.driver.find_element(
             By.CSS_SELECTOR, ".container > ul:nth-child(5) > li:nth-child(1)"
         )
-        date_text = invoice_date.text.split("</span>")[0].split(" ")[2]
-        invoice_data["invoice_date"] = date_text
-
-        invoice_due_date = self.driver.find_element(
-            By.CSS_SELECTOR,
-            ".container > ul:nth-child(5) > li:nth-child(2)",
+        due_date = self.driver.find_element(
+            By.CSS_SELECTOR, ".container > ul:nth-child(5) > li:nth-child(2)"
         )
-        due_date_text = invoice_due_date.text.split("</span>")[0].split(" ")[2]
-        invoice_data["invoice_due_date"] = due_date_text
+        return (
+            invoice_date.text.split(":")[1].strip(),
+            due_date.text.split(":")[1].strip(),
+        )
 
-        # Booking details
-        # table.table:nth-child(8)
+    def _extract_booking_details(self):
+        booking_data = {}
         booking_table = self.driver.find_element(
             By.CSS_SELECTOR, "table.table:nth-child(8)"
         )
         booking_rows = booking_table.find_elements(By.TAG_NAME, "tr")
         for row in booking_rows:
             columns = row.find_elements(By.TAG_NAME, "td")
-            label = columns[0].text
-            value = columns[1].text
-            invoice_data[label] = value
+            if len(columns) == 2:
+                booking_data[columns[0].text] = columns[1].text
+        return booking_data
 
-        # Table 'Booking/Stay details'
+    def _extract_stay_details(self):
+        stay_data = {}
         stay_table_rows = self.driver.find_elements(
             By.CSS_SELECTOR, "table:nth-of-type(1) tr"
         )
         for row in stay_table_rows:
             cells = row.find_elements(By.TAG_NAME, "td")
             if len(cells) == 2:
-                label = cells[0].text
-                value = cells[1].text
-                invoice_data[label] = value
+                stay_data[cells[0].text] = cells[1].text
+        return stay_data
 
-        # Customer details
+    def _extract_customer_details(self):
         customer_details = self.driver.find_element(
             By.CSS_SELECTOR, "html body section.content div.container div"
         )
-        invoice_data["customer_details"] = customer_details.text.replace("\n", ", ")
+        return customer_details.text.replace("\n", ", ")
 
-        # Billing Details
+    def _extract_billing_details(self):
+        billing_data = {}
         billing_rows = self.driver.find_elements(
             By.CSS_SELECTOR, "table:nth-of-type(2) tr"
         )
-        billing_headers = billing_rows[0].find_elements(By.TAG_NAME, "td")
-        billing_values = billing_rows[1].find_elements(By.TAG_NAME, "td")
-
-        for header, value in zip(billing_headers, billing_values):
-            invoice_data[header.text] = value.text
-
-        return invoice_data
+        headers = billing_rows[0].find_elements(By.TAG_NAME, "td")
+        values = billing_rows[1].find_elements(By.TAG_NAME, "td")
+        for header, value in zip(headers, values):
+            billing_data[header.text] = value.text
+        return billing_data
--- tests/test_invoices.py
+++ tests/test_invoices.py
@@ -5,17 +5,57 @@ from pages.invoices_list_page import InvoicesListPage
 from pages.invoice_detail_page import InvoiceDetailsPage
 
 
-# TODO read size from config
+def compare_dicts(expected, found):
+    differences = {
+        "mismatched_values": {},
+        "missing_in_found": {},
+        "extra_in_found": {},
+    }
+
+    for key in expected:
+        if key in found:
+            if expected[key] != found[key]:
+                differences["mismatched_values"][key] = {
+                    "expected": expected[key],
+                    "found": found[key],
+                }
+        else:
+            differences["missing_in_found"][key] = expected[key]
+
+    for key in found:
+        if key not in expected:
+            differences["extra_in_found"][key] = found[key]
+
+    return differences
 
 
-def test_count_invoices(logged_driver):
-    urls = InvoicesListPage(logged_driver).list_invoices()
-    assert len(urls) == 2, "Invoice list size not expected"
+expected_data = {
+    "hotel": "Rendezvous Hotel",
+    "invoice_id": "110",
+    "invoice_date": "14/01/2018",
+    "invoice_due_date": "15/01/2018",
+    "Booking Code": "0875",
+    "Room": "Superior Double",
+    "Total Stay Count": "1",
+    "Total Stay Amount": "$150",
+    "Check-In": "14/01/2018",
+    "Check-Out": "15/01/2018",
+    "customer_details": "JOHNY SMITH, R2, AVENUE DU MAROC, 123456",
+    "Deposit Nowt": "USD $20.90",  # Typo on page
+    "Tax&VAT": "USD $19",
+    "Total Amount": "USD $209",
+}
 
 
 def test_tc003_validate_invoice_details(logged_driver):
-    first_invoice_url = InvoicesListPage(logged_driver).list_invoices()[0]
-    invoice = InvoiceDetailsPage(logged_driver, first_invoice_url)
-    invoice_data = invoice.extract_all()
-    assert invoice_data != None
-    assert len(invoice_data) > 0
+    invoice_url = InvoicesListPage(logged_driver).list_invoices()[0]
+    invoice = InvoiceDetailsPage(logged_driver, invoice_url)
+    invoice_data = invoice.extract_all_details()
+    assert invoice_data != None, "Can't read invoice data"
+    differences = compare_dicts(expected_data, invoice_data)
+    mismatch = differences["mismatched_values"]
+    missing = differences["missing_in_found"]
+    extra = differences["extra_in_found"]
+    assert len(mismatch) == 0
+    assert len(missing) == 0
+    assert len(extra) == 0
