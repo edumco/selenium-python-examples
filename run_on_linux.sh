a882d850dc3c0b2baa1481bb55ddb6b7539739a0
José Eduardo Montenegro <edu.pombos@hotmail.com>
2024-11-08 08:36:54 -0300

    Script to run on linux

---
+++ run_on_linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Adds drivers to PATH
+export PATH=$PATH:$(pwd)/drivers/linux/chrome/chromedriver:$(pwd)/drivers/linux/firefox/geckodriver:$(pwd)/drivers/linux/edge/msedgedriver
+
+# creates virtual env
+python3 -m venv venv
+
+# Activate virtual env
+source venv/bin/activate
+
+# Install dependencies on virtual env
+pip install -r requirements.txt
+
+# Run tests
+python pytest --html=report.html
+
+# Prevent terminal from close 
+echo "Press any key to close ..."
+read -n 1 -s
