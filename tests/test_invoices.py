1824f1f0e95304ea281cd8227b2d45d5c46ee98b
José Eduardo Montenegro <edu.pombos@hotmail.com>
2024-11-08 04:10:04 -0300

    Basiic testing on invoice details

--- tests/test_invoices.py
+++ tests/test_invoices.py
@@ -2,13 +2,20 @@ import pytest
 from selenium import webdriver
 from pages.login_page import LoginPage
 from pages.invoices_list_page import InvoicesListPage
+from pages.invoice_detail_page import InvoiceDetailsPage
 
 
-# TODO read size from config on the running
-# TODO read valid user from config
-def test_count_invoices(driver):
-    login = LoginPage(driver).login("demouser", "abc123")
-    if login.is_authenticated():
-        invoices = InvoicesListPage(driver)
-        urls = invoices.list_invoices()
+# TODO read size from config
+
+
+def test_count_invoices(logged_driver):
+    urls = InvoicesListPage(logged_driver).list_invoices()
     assert len(urls) == 2, "Invoice list size not expected"
+
+
+def test_tc003_validate_invoice_details(logged_driver):
+    first_invoice_url = InvoicesListPage(logged_driver).list_invoices()[0]
+    invoice = InvoiceDetailsPage(logged_driver, first_invoice_url)
+    invoice_data = invoice.extract_all()
+    assert invoice_data != None
+    assert len(invoice_data) > 0
